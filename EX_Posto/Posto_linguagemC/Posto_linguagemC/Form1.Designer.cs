f35ec4c22efbcd518d5fe8757e32a47e135b5b47
Davi Nascimento <127548240+davin4sciment0@users.noreply.github.com>
2023-03-26 22:50:18 -0300

    ex_posto_C_1

---
+++ EX_Posto/Posto_linguagemC/Posto_linguagemC/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Posto_linguagemC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcomb = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.txtlitro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(240, 296);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(140, 23);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(410, 296);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(140, 23);
+            this.btnlimpar.TabIndex = 1;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o tipo de combustível:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DIgite a quantidade de litros:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor a ser pago:";
+            // 
+            // txtcomb
+            // 
+            this.txtcomb.Location = new System.Drawing.Point(450, 110);
+            this.txtcomb.Name = "txtcomb";
+            this.txtcomb.Size = new System.Drawing.Size(100, 23);
+            this.txtcomb.TabIndex = 6;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(450, 206);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 23);
+            this.txtvalor.TabIndex = 7;
+            // 
+            // txtlitro
+            // 
+            this.txtlitro.Location = new System.Drawing.Point(450, 157);
+            this.txtlitro.Name = "txtlitro";
+            this.txtlitro.Size = new System.Drawing.Size(100, 23);
+            this.txtlitro.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(283, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "A = Ácool / G = Gasolina";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtlitro);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.txtcomb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcomb;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.TextBox txtlitro;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EX_Posto/Posto_linguagemC/Posto_linguagemC/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Posto_linguagemC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double litro, valor, conta;
+            litro = Convert.ToDouble(txtlitro.Text);
+            valor = Convert.ToDouble(txtvalor.Text);
+
+            char comb;
+            comb = Convert.ToChar(txtcomb.Text);
+
+            if (comb = "A") {
+                if (litro = 20)
+                {
+                    conta = (litro * 14.7) / 100;
+                    valor = (litro * 4.90) - conta;
+                    txtvalor.Text = "O preço final com desconto será de: ", valor;
+                }
+                else
+                {
+                    conta = (litro * 24.5) / 100;
+                    valor = (litro * 4.90) - conta;
+                    txtvalor.Text = "O preço final com desconto será de: ", valor;
+                }
+            }
+            else
+            {
+                if (litro = 20)
+                {
+                    conta = (litro * 22) / 100;
+                    valor = (litro * 5.50) - conta;
+                    txtvalor.Text = "O preço final com desconto será de: ", valor;
+                }
+                else
+                {
+                    conta = (litro * 33) / 100;
+                    valor = (litro * 5.50) - conta;
+                    txtvalor.Text = "O preço final com desconto será de: ", valor;
+                }
+            }
+
+        }
+    }
+}
